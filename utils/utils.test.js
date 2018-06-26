683ea08d8f490e3da2dd44e82a4870aabc105a7f
Mahmoud <sswsy2006@gmail.com>
2018-06-26 19:29:19 +0200

    expect assertion library

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,15 +1,31 @@
+const expect = require('expect');
+
 const utils = require('./utils');
 
 it('should add two numbers', () => {
    let res = utils.add(5, 4);
-   if(res !== 9){
-      throw new Error(`expected 9, but got ${res}.`);
-   }
+   expect(res).toBe(9).toBeA('number');
 });
 
 it('should square a number', () => {
    let res = utils.square(3);
-   if(res !== 9){
-      throw new Error(`expected 9, but got ${res};`);
-   }
+   expect(res).toBe(9).toBeA('number');
 });
+
+it('should includes a user name', () => {
+   let user = {age: 26, location: "Alexandria"};
+   utils.setName(user, 'Mahmoud Ahmed');
+   expect(user).toInclude({firstName: 'Mahmoud'});
+   expect(user).toInclude({lastName: "Ahmed"});
+   expect(user).toBeA('object');
+});
+
+// it('should expect some values', () => {
+//    // expect({name: 'mahmoud'}).toNotEqual({name: 'Mahmoud'});
+//    // expect([1, 3, 5]).toInclude(5);
+//    expect({
+//       name: 'Mahmoud',
+//       age: 26,
+//       location: 'Alexandria'
+//    }).toExclude({age: 25});
+// });
